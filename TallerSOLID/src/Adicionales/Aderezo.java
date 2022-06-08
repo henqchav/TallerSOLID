7da6b432df1e48c2c50cf02da981f23ba9091ae2
aumoscos <andresulisesmoscoso@hotmail.com>
2022-06-08 17:55:12 -0500

    Punto 4, cambio de aderezo a abstracto, creacion de subclases

--- TallerSOLID/src/Adicionales/Aderezo.java
+++ TallerSOLID/src/Adicionales/Aderezo.java
@@ -9,7 +9,13 @@ package Adicionales;
  *
  * @author Pedro Mendoza
  */
-public enum Aderezo {
-    MALVAVISCOS,FRUTILLA,CREMA
+public abstract class Aderezo {
+    String nombre;
+    public abstract void setNombre();
+
+    public String toString(){
+        return this.nombre.toUpperCase();
+    }
     
 }
+
